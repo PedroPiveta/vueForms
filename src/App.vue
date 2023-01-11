185664808c0cb4a5f7bf1c645497eef8548bd1bd
PedroPiveta <ppivetabarrotti208@gmail.com>
2023-01-11 15:30:09 -0300

    first commit 😁👌

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import signupForm from './components/signupForm.vue'
+
+export default {
+  name: 'App',
+  components: { signupForm }
+}
+</script>
+
+<template>
+  <signupForm />
+</template>
+
+<style>
+body{
+  margin: 0;
+  background-color: #eee;
+}
+</style>
